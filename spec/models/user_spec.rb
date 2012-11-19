c23044c3f9a6b748b3bfa8285b68896d3aef7280
lessthancold <lessthancold@gmail.com>
2012-11-19 12:12:44 +0400

    Finished first cut of the User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -35,4 +35,28 @@ it "should reject names that are too long" do
 	long_name_user = User.new(@attr.merge(name: long_name))
 	long_name_user.should_not be_valid
 end
+
+it "should accept valid email addresses" do
+	addresses = %w[user@foo.com THE_USER@foo.bar.org example.user@foo.jp]
+	addresses.each do |address|
+		valid_email_user = User.new(@attr.merge(:email => address))
+		valid_email_user.should be_valid
+	end
+end
+
+it "should reject invalid email addresses" do
+	addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+	addresses.each do |address|
+		invalid_email_user = User.new(@attr.merge(:email => address))
+		invalid_email_user.should_not be_valid
+	end
+end
+
+it "should reject duplicate email addresses no matter case" do
+	upcased_email = @attr[:email].upcase
+	User.create!(@attr.merge(email: upcased_email))
+	user_with_duplicate_email = User.new(@attr)
+	user_with_duplicate_email.valid?
+end
+
 end
